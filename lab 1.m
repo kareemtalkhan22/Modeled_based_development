0b086830616a6c514c16a1735ee2452bd63252e4
kareemtalkhan22 <157373094+kareemtalkhan22@users.noreply.github.com>
2024-01-31 19:34:15 +0200

    Add files via upload

---
+++ lab 1.m
@@ -0,0 +1,10 @@
+a=[1 2 3;4 5 6;7 8 9];
+b=[10 11 12;13 14 15;16 17 18];
+matsum=plus(a,b)
+%%.
+rowvector=1:5;
+columvector=6:10;
+horzcat=cat(2,rowvector,columvector)
+%%.
+x=[5 6;2 3];
+y=repmat(x,2,2)
